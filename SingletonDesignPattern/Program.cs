f06e1ab2298972e6c79ef453a7beb7f993cf90dd
Dinçer Şipka <dincersipka6@gmail.com>
2023-03-20 12:07:16 +0300

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SingletonDesignPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton Example_1 = Singleton.GetInstance();
+            Singleton Example_2 = Singleton.GetInstance();
+
+            Console.WriteLine(Example_1 == Example_2);
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton Instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance() 
+        {
+            if (Instance == null) 
+            {
+                Instance = new Singleton();
+            }
+
+            return Instance;
+        }
+    }
+}
